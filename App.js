c88b13bf6a4bcb9e67d1a0b9f16aceab7cb9a31f
hicham tijani <hicham.tijani21@gmail.com>
2023-11-14 23:10:06 +0100

    Uber

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {createStackNavigator} from '@react-navigation/stack'
+
 
 export default function App() {
+const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name='HomeScreen'
+        component={HomeScreen}
+        options={{
+          headerShow:false,
+        }}
+        />
+        <Stack.Screen 
+        name='MapScreen'
+        component={MapScreen}
+        options={{
+          headerShow:false,
+        }}
+        />
+      </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ slices/navSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+ origin: null,
+ destination: null,
+ travelTimeInformation:null
+}
+
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+        state.origin = action.playload;
+    },
+    setDestination: (state, action) => {
+        state.destination = action.playload;
+    },
+    setTravelTimeInformation: (state, action) => {
+        state.travelTimeInformation = action.playload;
+    },
+  },
+});
+
+
+export const {setOrigin, setDestination,
+     setTravelTimeInformation} = navSlice.actions;
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestnation = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
